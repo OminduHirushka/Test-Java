8f8b9a4fd0b42651ab79384dd45ca8be0bff10a1
Omindu Hirushka <ominduhirushka7@gmail.com>
2024-03-04 11:37:27 +0530

    Initial Commit

---
+++ Java/src/A2/Q1.java
@@ -0,0 +1,16 @@
+package A2;
+
+public class Q1 {
+    public static void main(String[] args) {
+        int rows = 5;
+
+        // Outer loop to iterate through rows
+        for (int i = 1; i <= rows; i++) {
+            // Inner loop to print * in each column
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*   ");
+            }
+            System.out.println(); // Move to the next line after printing each row
+        }
+    }
+}
