069ddb63a3c35db2f5676e65cc23cc31da692cc7
Omindu Hirushka <ominduhirushka7@gmail.com>
2024-03-12 23:57:29 +0530

    Q1-Q8 Done

---
+++ Java/src/A3/Q4.java
@@ -0,0 +1,13 @@
+package A3;
+
+public class Q4 {
+    public static void main(String[] args) {
+        System.out.println(10 + 20 + 30);
+        System.out.println("10+20+30");
+        System.out.println("10+20" + 30);
+        System.out.println("10" + "20" + "30");
+        System.out.println("10" + 20 + 30);
+        System.out.println(10 + 20 + "30");
+        System.out.println(10 + "20" + 30);
+    }
+}
---
+++ Java/src/A3/Q8.java
@@ -0,0 +1,16 @@
+package A3;
+
+import java.util.Scanner;
+
+public class Q8 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Input your age : ");
+        int age=input.nextInt();
+        System.out.println("Your current age is : "+age);
+
+        age = age + 10;
+        System.out.println("Your age after 10 years is : " +age);
+    }
+}
